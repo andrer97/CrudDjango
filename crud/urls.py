4ac3a02b7f23418667e1e528cad642c61f0077c1
andrer97 <andre.rossano@hotmail.com>
2024-04-12 21:38:34 +0000

    Atualizações páginas

--- crud/urls.py
+++ crud/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, criar, editar, atualizar, deletar
+from .views import home, create, criar, editar, atualizar, update, deletar, delete  
 
 urlpatterns = [
     path('', home, name="home"),
+    path('create', create, name="create"),
     path('criar', criar, name="criar"),
     path('editar/<int:id>', editar, name="editar"),
-    path('atualizar/<int:id>', atualizar, name="atualizar"),
-    path('deletar/<int:id>', deletar, name="deletar"),
+    path('atualizar', atualizar, name="atualizar"),
+    path('update/<int:id>', update, name="update"),
+    path('deletar', deletar, name="deletar"),
+    path('delete/<int:id>', delete, name="delete"),
 ]
--- crud/views.py
+++ crud/views.py
@@ -8,6 +8,10 @@ def home(request):
     return render(request, "crud/global/index.html", {"usuarios": usuarios})
 
 def criar(request):
+    usuarios = Usuario.objects.all()
+    return render(request, "crud/global/criar.html", {"usuarios": usuarios})
+
+def create(request):
     nome = request.POST.get("nome")
     dataNascimento = request.POST.get("dataNascimento")
     email = request.POST.get("email")
@@ -20,7 +24,11 @@ def editar(request, id):
     usuario = Usuario.objects.get(id=id)
     return render(request, "crud/global/editar.html", {"usuario": usuario})
 
-def atualizar(request, id):
+def atualizar(request):
+    usuarios = Usuario.objects.all()
+    return render(request, "crud/global/atualizar.html", {"usuarios": usuarios})
+
+def update(request, id):
     nome = request.POST.get("nome")
     dataNascimento = request.POST.get("dataNascimento")
     email = request.POST.get("email")
@@ -33,7 +41,11 @@ def atualizar(request, id):
     usuario.save()
     return redirect(home)
 
-def deletar(request, id):
+def deletar(request):
+    usuarios = Usuario.objects.all()
+    return render(request, "crud/global/deletar.html", {"usuarios": usuarios})
+
+def delete(request, id):
     usuario = Usuario.objects.get(id=id)
     usuario.delete()
     return redirect(home)
